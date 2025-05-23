daf8aecf79ad28105b2a54e5e60f435253d6c1d7
Mariosam2 <marco.mariosaa@gmail.com>
2025-05-23 17:48:50 +0200

    basic initialization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'product/:productUUID', component: ShopComponent },
+  { path: 'checkout', component: CheckoutComponent },
+];
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+interface Link {
+  path: string;
+  title?: string;
+  routeParam?: string;
+}
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  links: Link[] = [
+    { path: '', title: 'home' },
+    { path: '/shop', title: 'shop' },
+    { path: '/product', routeParam: '' },
+    { path: '/checkout' },
+  ];
+}
